11ad66e662a7c27143ef3c627d09607949a079b9
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 06:39:50 +0900

    fix: Substituting a custom User model

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+# for Substituting a custom User model
+AUTH_USER_MODEL = 'users.User'
+
 try:
     from .local_settings import *
 except ImportError:
