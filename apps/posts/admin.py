29d171c5ee003743b0bbc7056c76e47bfd8c2155
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 06:23:22 +0900

    fix: modify admin.py in posts

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from apps.posts.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'rate', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+admin.site.register(Post, PostAdmin)
