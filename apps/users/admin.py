c36ac94594d68802ea46d5110288a6c159e36a46
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 06:23:09 +0900

    fix: modify admin.py in users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined')
+    list_filter = ['date_joined']
+    search_fields = ['username']
+
+
+admin.site.register(User, UserAdmin)
