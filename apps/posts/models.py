5282fd40081dc76c6d60b63a265f4794efd7da95
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 06:17:09 +0900

    feature: add posts model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+
+    RATES = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+    )
+
+    title = models.CharField(max_length=150)
+    body = models.TextField(blank=True)
+    country = models.CharField(max_length=150, blank=True)
+    pub_date = models.DateTimeField('date published')
+    image_path = models.ImageField(upload_to='', blank=True, null=True, default='noimage.png')
+    image_path2 = models.ImageField(upload_to='', blank=True, null=True, default='noimage.png')
+    image_path3 = models.ImageField(upload_to='', blank=True, null=True, default='noimage.png')
+    image_path4 = models.ImageField(upload_to='', blank=True, null=True, default='noimage.png')
+    image_path5 = models.ImageField(upload_to='', blank=True, null=True, default='noimage.png')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rate = models.IntegerField(choices=RATES)
+
+    def __str__(self):
+        return self.title
