c03cafb1cf6b5b0c8561089baa43da8710839d14
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 06:17:25 +0900

    feature: add users model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(UserManager):
+    pass
+
+
+class User(AbstractUser):
+    username = models.CharField(_('username'), max_length=150, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+    thumbnail_path = models.ImageField(upload_to='', blank=True, null=True, default='no-user.jpg')
+    bio = models.TextField(max_length=150, blank=True)
+
+    objects = UserManager()
