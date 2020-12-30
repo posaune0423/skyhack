28a57c8d7f3b6f76fc1d286b4cb4c4c923de1626
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 20:51:54 +0900

    feature: add functions according to the REST

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -22,3 +22,44 @@ def show(request, id):
         .first()
 
     return render(request, 'posts/show.html', {'post': post})
+
+
+def create(request, params):
+    post = {
+        'title': params['title'],
+        'body': params['body'],
+        'pub_date': params['pub_date'],
+        'country': params['country'],
+        'image_path': params['image_path'],
+        'image_path2': params['image_path2'],
+        'image_path3': params['image_path3'],
+        'image_path4': params['image_path4'],
+        'image_path5': params['image_path5'],
+        'author_id': params['author_id'],
+        'rate': params['rate'],
+    }
+
+    return Post\
+        .insert(post)
+
+
+def update(request, id, params):
+    params = {
+        'title': params['title'],
+        'body': params['body'],
+        'country': params['country'],
+        'image_path': params['image_path'],
+        'image_path2': params['image_path2'],
+        'image_path3': params['image_path3'],
+        'image_path4': params['image_path4'],
+        'image_path5': params['image_path5'],
+        'rate': params['rate'],
+    }
+
+    return Post\
+        .where('id', id)\
+        .update(params)
+
+
+def delete(request, id):
+    Post.where('id', id).delete()
