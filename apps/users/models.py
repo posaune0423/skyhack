6b1920d5b70c9b24f87678a1743b39a547138856
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 17:10:27 +0900

    fix: remove app_name from db_name

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,11 +2,6 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
-class UserManager(UserManager):
-    pass
-
-
 class User(AbstractUser):
     username = models.CharField(_('username'), max_length=150, unique=True)
     email = models.EmailField(_('email address'), unique=True)
@@ -14,3 +9,7 @@ class User(AbstractUser):
     bio = models.TextField(max_length=150, blank=True)
 
     objects = UserManager()
+
+    class Meta:
+        db_table = 'users'
+        verbose_name_plural = 'Users'
