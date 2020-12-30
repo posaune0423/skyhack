c9cbf58588266e7d97cb9832b65817c3b89ddf70
Asuma Yamada <y.asuma0423@icloud.com>
2020-12-30 19:03:28 +0900

    feature: create views

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from apps.orater.post import Post
+
+
 def top(request):
     return render(request, 'index.html')
 
-def index(request):
-    return render(request, 'posts/index.html')
 
-def show(request):
-    return render(request, 'posts/show.html')
+def index(request):
+    posts = Post \
+        .order_by('pub_date', 'desc') \
+        .limit(5) \
+        .get()
+
+    return render(request, 'posts/index.html', {'posts': posts})
+
+
+def show(request, id):
+    post = Post \
+        .where('id', id) \
+        .first()
+
+    return render(request, 'posts/show.html', {'post': post})
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from apps.orater.user import User
+from apps.orater.post import Post
+
+
+def show(request, id):
+    user = User.find(id)
+    posts = Post\
+        .where('author_id', id)\
+        .order_by('pub_date', 'desc')\
+        .get()
+
+
+    return render(request, 'users/show.html', {'user': user, 'posts': posts})
