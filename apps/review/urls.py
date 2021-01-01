7f2daecc4866170cc4bce96228d2b7710950c600
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-01 18:51:54 +0900

    refactor: change views from class base one to func base one

--- apps/review/urls.py
+++ apps/review/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-
 import apps.review.views
 
 urlpatterns = [
-    path('<int:pk>/edit', login_required(apps.review.views.Update.as_view())),
-    path('create/', login_required(apps.review.views.Create.as_view())),
-    path('<int:pk>/delete', login_required(apps.review.views.delete)),
+    # path('<int:pk>/edit', login_required(apps.review.views.Update.as_view())),
+    # path('create/', login_required(apps.review.views.Create.as_view())),
+    path('<int:id>/edit', apps.review.views.update),
+    path('create/', apps.review.views.create),
+    path('<int:id>/delete', apps.review.views.delete),
 ]
--- apps/review/views.py
+++ apps/review/views.py
@@ -1,27 +1,46 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
 from apps.review.forms import ReviewForm
-from apps.review.models import Review
+from apps.orater.review import Review
 
 
-class Create(CreateView):
-    model = Review
-    form_class = ReviewForm
-    template_name = 'review/create.html'
-    success_url = '/mypage/'
-
-
-class Update(UpdateView):
-    model = Review
-    form_class = ReviewForm
-    template_name = 'review/edit.html'
-
-
-def delete(request, pk):
-    post = get_object_or_404(Review, id=pk)
-    if (request.user.id != post.author_id):
+@login_required
+def create(request):
+    form = ReviewForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
         return redirect('/mypage/')
 
-    post.delete()
+    return render(request, 'review/create.html', {'form': form})
+
+
+@login_required
+def update(request, id):
+    form = ReviewForm(request.POST)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('/mypage/')
+
+    review = Review.find(id)
+    fields = {
+        "title": review.title,
+        'body': review.body,
+        "created_at": review.created_at,
+        "author": review.author_id,
+        "airport": review.airport_id,
+        "rate": review.rate,
+    }
+
+    form = ReviewForm(fields)
+    return render(request, 'review/edit.html', {'form': form})
+
+
+@login_required
+def delete(request, id):
+    review = Review.find(id)
+    if (request.user.id != review.author_id):
+        return redirect('/mypage/')
+
+    review.delete()
     return redirect('/mypage/')
