eb8f2082e4e11a56a9515034143b36e9358b4f4f
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-01 22:46:11 +0900

    fix: remove STATIC_ROOT setting

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # staticfiles conf
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # media conf
