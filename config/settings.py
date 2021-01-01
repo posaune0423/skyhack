9afb6afd18d74643c010e52c77c7f0ec6c2430ce
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-02 00:56:17 +0900

    refactor: fix settings

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # static conf
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
