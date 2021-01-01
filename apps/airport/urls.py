7e3fe6422d8acf59f4c33c6e12599631659f52c5
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-01 18:50:09 +0900

    refactor: change views from class base one to func base one

--- apps/airport/urls.py
+++ apps/airport/urls.py
@@ -5,5 +5,6 @@ from apps.airport import views
 app_name = 'airport'
 urlpatterns = [
     path('<int:pk>', views.show),
-    path('search/', login_required(views.Search.as_view())),
+    # path('search/', login_required(views.Search.as_view())),
+    path('search/', views.search),
 ]
