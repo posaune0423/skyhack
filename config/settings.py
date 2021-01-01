17a6993d0cb18163fed563f5ec802fcb449aa9a1
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-02 01:04:32 +0900

    refactor: fix settings

--- config/settings.py
+++ config/settings.py
@@ -51,15 +51,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    # add whitenoise for loading static files in Heroku
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # add whitenoise for loading static files in Heroku
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
