56e43d5157ba91853464c5cf306c2cb03a71176e
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-01 20:57:40 +0900

    refactor: make thumbnail unnullable

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     username = models.CharField(_('username'), max_length=150, unique=True)
     email = models.EmailField(_('mail address'), unique=True)
-    thumbnail = models.ImageField(upload_to='', blank=True, null=True, default='no-user.jpg')
+    thumbnail = models.ImageField(upload_to='', default='no-user.jpg')
     bio = models.TextField(max_length=150, blank=True)
 
     objects = UserManager()
