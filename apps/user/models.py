166f676148efd7872195e258c3d95bfbbb7b3b6d
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-09 04:43:18 +0900

    feature: change ImageFiled to CloudinaryField in user.model

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class User(AbstractUser):
     username = models.CharField('username', max_length=150, unique=True)
     email = models.EmailField('mail address', unique=True)
-    thumbnail = CloudinaryField('media/', blank=True, null=True, default='v1610122704/media/no-user_b9riju.jpg')
+    thumbnail = CloudinaryField('Thumbnail', blank=True, null=True, default='v1610122704/media/no-user_b9riju.jpg')
     bio = models.TextField(max_length=150, blank=True)
 
     objects = UserManager()
