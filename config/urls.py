63edfa7df99a4bf93dc4185b26a43acd4ac47596
Asuma Yamada <y.asuma0423@icloud.com>
2021-01-09 04:40:56 +0900

    refactor: remove media root pat from urlpattern

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,8 @@
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
-from django.conf.urls.static import static
+
 
 import apps.airport.views
 import apps.user.views
@@ -18,4 +17,4 @@ urlpatterns = [
                   path('reviews/', include('apps.review.urls')),
                   path('mypage/', include('apps.mypage.urls')),
                   path('', apps.airport.views.top),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]
