8e5555bf2b5f1ab58977cecf7e73f5000dae4717
Asuma Yamada <yamadaasuma@gmail.com>
2021-03-21 03:11:31 +0900

    refactor: commnet out

--- apps/orator/__init__.py
+++ apps/orator/__init__.py
@@ -1,9 +1,9 @@
-import logging
-# import environ
-
-from orator import DatabaseManager
-from orator import Model
-from django.conf import settings
+# import logging
+# # import environ
+#
+# from orator import DatabaseManager
+# from orator import Model
+# from django.conf import settings
 
 
 # env = environ.Env()
@@ -20,28 +20,28 @@ from django.conf import settings
 #         'log_queries': True,
 #     }
 # }
-config = {
-    'default': 'database',
-    'database': {
-        'driver': 'sqlite',
-        'database': settings.DATABASES['default']['NAME'],
-        'log_queries': True,
-    }
-}
-
-
-db = DatabaseManager(config)
-Model.set_connection_resolver(db)
-
-
-logger = logging.getLogger('orator.connection.queries')
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter(
-    'It took %(elapsed_time)sms to execute the query %(query)s'
-)
-
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+# config = {
+#     'default': 'database',
+#     'database': {
+#         'driver': 'sqlite',
+#         'database': settings.DATABASES['default']['NAME'],
+#         'log_queries': True,
+#     }
+# }
+#
+#
+# db = DatabaseManager(config)
+# Model.set_connection_resolver(db)
+#
+#
+# logger = logging.getLogger('orator.connection.queries')
+# logger.setLevel(logging.DEBUG)
+#
+# formatter = logging.Formatter(
+#     'It took %(elapsed_time)sms to execute the query %(query)s'
+# )
+#
+# handler = logging.StreamHandler()
+# handler.setFormatter(formatter)
+#
+# logger.addHandler(handler)
